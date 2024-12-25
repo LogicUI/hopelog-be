8096d858d0c3b03db39f41a8c1c9509eed423fb4
LogicUI <johnwee35@gmail.com>
2024-12-25 23:39:29 +0800

    update supabase_init.py

--- app/supabase_init.py
+++ app/supabase_init.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from supabase import create_client, Client
-
+import logging
 import os
 
 
@@ -10,6 +10,11 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_KEY")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+logging.info("supabase url: %s", SUPABASE_URL)
+logging.info("supabase key: %s", SUPABASE_ANON_KEY)
+logging.info("frontend url: %s", FRONTEND_URL)
+
 
 if not SUPABASE_URL or not SUPABASE_ANON_KEY:
     raise ValueError("Supabase URL or Key is missing")
