3954411e42c6e001e5a66cbed753fd46725fa703
LogicUI <johnwee35@gmail.com>
2025-01-05 00:07:55 +0800

    update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,9 +1,2 @@
-if [ -z "$PORT" ]; then
-  # If PORT is not set, assume it's local development
-  echo "Running in local development mode..."
+  echo "Running in  development mode..."
   uvicorn main:app --host 0.0.0.0 --port 5000 --reload
-else
-  # If PORT is set, assume it's Heroku or production
-  echo "Running in development mode on port $PORT..."
-  uvicorn main:app --host 0.0.0.0 --port $PORT
-fi
